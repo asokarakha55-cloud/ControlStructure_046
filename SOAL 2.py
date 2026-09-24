edbc6c95336ea899a03386fa0e50cf95b5c801bc
asokarakha55-cloud <asokarakha55@gmail.com>
2026-09-24 10:52:56 +0700

    Membuat sistem pencari angka terbesar

---
+++ SOAL 2.py
@@ -0,0 +1,12 @@
+1 = int(input("Masukkan angka pertama: "))
+2 = int(input("Masukkan angka kedua: "))
+3 = int(input("Masukkan angka ketiga: "))
+
+if 1 >= 2 and 1 >= 3:
+    terbesar = 1
+elif 2 >= 1 and 2 >= 3:
+    terbesar = 2
+else:
+    terbesar = 3
+
+print(f"Angka terbesar adalah: {terbesar}")
